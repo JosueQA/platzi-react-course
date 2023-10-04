17b1e702dcfc87b68ad17d3f76c6aa59666018eb
p <josuelml28@hotmail.com>
2023-10-04 14:58:44 -0500

    Avance de la llamada a la API

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,7 +1,15 @@
+import { useState, useEffect } from "react";
 import Layout from "../../Components/Layout";
 import Card from "../../Components/Card";
 
 function Home() {
+  const [item, setItem] = useState(null)
+  
+  useEffect (() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(response => console.log(response.json()))
+  }, []) 
+
   return (
     <Layout>
       <Card />
